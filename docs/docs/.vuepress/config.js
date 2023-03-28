58a2fd4aa5187bdd29f2fc71c89d9d3233a0ab6c
cvSolider <610496306@qq.com>
2023-03-28 16:26:51 +0800

    自定义主题和博客页navbar

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,6 +2,8 @@ import { hopeTheme } from "vuepress-theme-hope";
 
 export default {
   theme: hopeTheme({
+    repo: "cvSoldier/keep-being-curious/tree/master/docs",
+    repoLabel: "GitHub",
     plugins: {
       blog: {
           excerptLength: 0
